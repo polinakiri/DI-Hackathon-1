5f3cc1dd082ba25fa3573e3df1b58170825ccca7
Polina <polina.a.kiri@gmail.com>
2023-02-02 16:24:34 +0200

    added game function

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ const words = [
       word = randomItem.word;
       hint = randomItem.hint;
       hintPara.appendChild(document.createTextNode(hint));
-  
+      console.log(word);
+
       let hiddenLetters = "";
       for (let i = 0; i < word.length; i++) {
           hiddenLetters +=`<input type="text" disabled>`;
@@ -28,3 +29,27 @@ const words = [
   }
   
   randomWord();
+
+  const form = document.forms[0];
+  form.addEventListener("submit", game);
+
+  
+
+  function game (event) {
+    event.preventDefault();
+
+    const userLetter = document.querySelector(".letter").value;
+
+    const cell = document.querySelector(".random-word");
+   
+    if (word.match(userLetter) != null) {
+        let correctLetter = word.match(userLetter)[0];
+        const indx = word.indexOf(correctLetter);
+        cell.childNodes[indx].value = correctLetter;
+        console.log(cell.childNodes[indx]);
+    } else {
+        const listOfTries = document.querySelector(".tries");
+        listOfTries.appendChild(document.createTextNode(`${userLetter} `));
+        console.log(listOfTries);
+    }
+  }
