305233d8c2f90b8164774c3564e8678d2bea6196
Polina <polina.a.kiri@gmail.com>
2023-02-05 13:35:24 +0200

    everything is fixed!

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 const words = [
-    { word: "javascript", hint: "programming language" },
     { word: "guitar", hint: "a musical instrument" },
     { word: "venus", hint: "planet of our solar system" },
     { word: "gold", hint: "a yellow precious metal" },
     { word: "coding", hint: "related to programming" },
     { word: "gif", hint: "a file format for image" },
     { word: "island", hint: "land surrounded by water" },
-    { word: "chess", hint: "related to an indoor game" }
+    { word: "python", hint: "programming language"}, 
+    { word: "aim", hint: "a purpose or intention"},
+    { word: "ebay", hint: "online shopping site"},
+    { word: "bugs", hint: "related to programming"},
+    { word: "mental", hint: "related to the mind"},
+    { word: "map", hint: "diagram represent of an area"},
+    { word: "hockey", hint: "a popular outdoor game"},
+    { word: "github", hint: "code hosting platform"},
+    { word: "silver", hint: "precious greyish-white metal"},
+    { word: "email", hint: "related to exchanging message"},
+    { word: "html", hint: "markup language for the web"},
+    { word: "idea", hint: "a thought or suggestion"},
+    { word: "rain", hint: "related to a water"}
   ];
   
-  let word = [];
+  let word;
   let hint;
   const randomWordBox = document.querySelector(".random-word");
   const hintPara = document.querySelector(".hint");
+
+  const msg = document.querySelector(".message");
+
+  const remPara = document.querySelector(".remaining");
+
+  const listOfTries = document.querySelector(".tries");
   
   function randomWord () {
+    msg.innerText = `Let's play!`;
+    remPara.innerText = `Remaining guesses: `
+    listOfTries.innerText = `Wrong letters: `
       const randomItem = words[Math.floor(Math.random() * words.length)];
       word = randomItem.word;
-      hint = randomItem.hint;
-      hintPara.appendChild(document.createTextNode(hint));
+      hintPara.innerText = `Hint: ${randomItem.hint}`;
       console.log(word);
 
       let hiddenLetters = "";
@@ -42,45 +61,41 @@ const words = [
 
     const cell = document.querySelector(".random-word");
 
-    let msg = document.querySelector(".message");
-
-    const listOfTries = document.querySelector(".tries");
-
     const li = document.createElement("li");
    
     if (word.match(userLetter) != null) {
         let correctLetter = word.match(userLetter)[0];
         const indx = word.indexOf(correctLetter);
         cell.childNodes[indx].value = correctLetter;
-        msg.innerText = `Good guess! The word has the letter ${userLetter}`;
+        msg.innerText = `Good guess! The word has the letter ${userLetter.toUpperCase()}`;
     } else {
         li.appendChild(document.createTextNode(`${userLetter} `));
         listOfTries.appendChild(li);
-        msg.innerText = `Oh no! The word has not the letter ${userLetter}`;
+        msg.innerText = `Oh no! The word has not the letter ${userLetter.toUpperCase()}`;
     }
 
-    let maxNumOfTries = word.length + 4;
+    let maxNumOfTries = word.length + 2;
     let numOfTries = listOfTries.childNodes.length - 1;
     const remaining = maxNumOfTries - numOfTries;
 
-    const remPara = document.querySelector(".remaining");
-    remPara.append(document.createTextNode(remaining));
+    remPara.innerText = `Remaining guesses: ${remaining}`;
 
-//     let allInputs = cell.querySelectorAll("input");
-//     // console.log(allInputs);
+    let allInputs = cell.querySelectorAll("input");
+    let lettersFromInputs = "";
+    for (let i =0; i < word.length; i++) {
+        if (allInputs[i].value != "") {
+        lettersFromInputs += allInputs[i].value;
+    }
+    }
 
+    if (remaining === 0) {
+        alert(`Game over! You don't have remaining guesses.`);
+        location.reload();
+    } else if(lettersFromInputs.length === allInputs.length) {
+        alert(`WINNER! You found the word ${word.toUpperCase()}.`);
+        location.reload();
+    } 
+}
 
-//     for (let x of allInputs) {
-//     console.log(x.value);
-//     }
-
-//     if (remaining === 0) {
-//         alert("Game over");
-//     } else if (x.value != null) {
-//         alert("winner")
-//     }
-//   }
-
-
-  const newGame = document.querySelector(".start-btn");
-  newGame.addEventListener("click", randomWord);
+const newGame = document.querySelector(".start-btn");
+newGame.addEventListener("click", randomWord);
